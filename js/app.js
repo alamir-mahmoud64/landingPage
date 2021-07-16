64802187b297a7fef40ec8c9811816eb9d77ae8f
Alamir Mahmoud <alamir.mahmoud64@gmail.com>
2021-07-17 00:47:01 +0300

    build nav, scroll to sectoin and distinguish active section

--- js/app.js
+++ js/app.js
@@ -17,7 +17,9 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll('section');
+const newFragment = document.createDocumentFragment();
+const navList = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
@@ -27,6 +29,37 @@
 
 
 
+function scrollToSection(event){
+    event.preventDefault();
+    const secId = event.target.getAttribute('href');
+    const section=document.querySelector(secId);
+    section.scrollIntoView({behavior: 'smooth', block: 'center'}); 
+}
+
+function toggleActiveState(){
+    let activeSectionId = null;
+    const allAnchors = document.querySelectorAll('a');
+    for(const anchor of allAnchors){
+        anchor.classList.remove('active-anchor');
+    }
+    for(const section of sections){
+        const viewProps = section.getBoundingClientRect();
+        if(viewProps.top>=0 && viewProps.top<=500){
+            section.classList.add('your-active-class');
+            activeSectionId = section.getAttribute('id');
+        }else{
+            section.classList.remove('your-active-class');
+        }
+    }
+    for (const anchor of allAnchors) {
+        if(activeSectionId!==null && anchor.getAttribute('href')==="#"+activeSectionId){
+            anchor.classList.add('item-active-class');
+        }else{
+            anchor.classList.remove('item-active-class');
+        }
+    }
+}
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -34,7 +67,20 @@
 */
 
 // build the nav
-
+function buildNavigationMenu(){
+    for (const section of sections) {
+        const liElement = document.createElement('li');
+        const anchor = document.createElement('a');
+        const anchorText = document.createTextNode(section.getAttribute('data-nav'));
+        anchor.setAttribute('href',"#"+section.getAttribute('id'));
+        anchor.appendChild(anchorText);
+        anchor.addEventListener('click',scrollToSection);
+        anchor.classList.add('menu__link');
+        liElement.appendChild(anchor);
+        newFragment.appendChild(liElement);
+    }
+    navList.appendChild(newFragment);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -55,3 +101,4 @@
 // Set sections as active
 
 
+window.addEventListener('scroll', toggleActiveState); 
